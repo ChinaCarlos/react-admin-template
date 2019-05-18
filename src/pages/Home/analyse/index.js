7f266e149b6747f413b3093eeff43aa583b39266
maliaoMJ <1302151931@qq.com>
2019-05-19 00:00:25 +0800

    添加styled-components

--- src/pages/Home/analyse/index.js
+++ src/pages/Home/analyse/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
+
+const Button = styled.div`
+  width: 180px;
+  height: 40px;
+  background: red;
+  color: #fff;
+  text-align: center;
+  line-height: 40px;
+`
 class Analyse extends React.Component {
   render() {
-    return <div>Analyse</div>
+    return (
+      <div>
+        <Button>styledComponents</Button>
+      </div>
+    )
   }
 }
 
