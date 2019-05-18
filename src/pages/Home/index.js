ecfac7335fcd884a16316cdd8617f2ca36ece86d
maliaoMJ <1302151931@qq.com>
2019-05-18 20:18:37 +0800

    添加分割代码的功能

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component, Suspense, lazy } from 'react'
 import {
   BrowserRouter as Router,
   Route,
@@ -9,25 +9,26 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import DefaultLayout from '../../layuot/default/'
 // 引入该模块下的子页面
-import Analyse from './analyse/'
-import Workplace from './workplace/'
-
+const Analyse = lazy(() => import('./analyse/'))
+const Workplace = lazy(() => import('./workplace/'))
 class Index extends React.Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route
-            exact
-            path="/home"
-            render={() => {
-              return <Redirect to="/home/analyse" />
-            }}
-          />
-          <Route path="/home/analyse" component={Analyse} />
-          <Route path="/home/workplace" component={Workplace} />
-        </Switch>
-      </Router>
+      <Suspense fallback={<div>loading</div>}>
+        <Router>
+          <Switch>
+            <Route
+              exact
+              path="/home"
+              render={() => {
+                return <Redirect to="/home/analyse" />
+              }}
+            />
+            <Route path="/home/analyse" component={Analyse} />
+            <Route path="/home/workplace" component={Workplace} />
+          </Switch>
+        </Router>
+      </Suspense>
     )
   }
 }
