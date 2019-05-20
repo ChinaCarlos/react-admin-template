815f0bb52d5271ab9520c2ea41a9274c21c047dc
maliaoMJ <1302151931@qq.com>
2019-05-20 22:52:21 +0800

    修复构建引用过路径错误的问题

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default class Login extends React.Component {
   render() {
-    return <div>Login page</div>
+    return (
+      <div>
+        <Link to="/home/analyse">分析页</Link>
+        <Link to="/home/workplace">工作台</Link>
+      </div>
+    )
   }
 }
--- start.js
+++ start.js
@@ -3,14 +3,14 @@ const path = require('path')
 const express = require('express')
 const fs = require('fs')
 const app = express()
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, './build')))
 app.use(function(req, res) {
   fs.readFile(
-    path.join(__dirname, 'build/index.html'),
+    path.join(__dirname, './build/index.html'),
     'utf-8',
     (err, content) => {
       if (err) {
-        console.log('We Cannot open "bind/index.html" file.')
+        console.log('We Cannot open "buind/index.html" file.')
       }
       res.writeHead(200, {
         'Content-Type': 'text/html; charset=utf-8'
@@ -19,6 +19,6 @@ app.use(function(req, res) {
     }
   )
 })
-app.listen('6666', function() {
+app.listen('8080', function() {
   console.log('启动服务器完成')
 })
